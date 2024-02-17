36114ac0631b5d28add281c40e841026a3c570b7
toyokokichi <koki0515baske@gmail.com>
2024-02-17 16:54:28 +0900

    新規登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,5 +10,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :katakana_last_name, :katakana_first_name, :birthdate])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :katakana_last_name, presence: true
+  validates :katakana_first_name, presence: true
+  validates :birthdate, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
 end
