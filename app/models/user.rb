e6c8a34005aa7062f434fc53c00fbdc4785cf79b
toyokokichi <koki0515baske@gmail.com>
2024-02-25 12:18:35 +0900

    正規表現、テストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :katakana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :katakana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthdate, presence: true
