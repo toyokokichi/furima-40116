b508a1c1a5138e79cb030d58529afa79800c26bc
toyokokichi <koki0515baske@gmail.com>
2024-02-25 17:09:56 +0900

    バリーデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,20 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_detail
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  validates :content, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_detail_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true,
+                    numericality: {
+                      greater_than_or_equal_to: 300,
+                      less_than_or_equal_to: 9999999,
+                      only_integer: true
+                                      }
+  validates :user_id, presence: true, foreign_key: true
 end
