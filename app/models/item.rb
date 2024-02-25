8144659c377ced8aadc17aa102034dc57e24067d
toyokokichi <koki0515baske@gmail.com>
2024-02-25 15:36:20 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_detail
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
