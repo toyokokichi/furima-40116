a556c17f1d7f0fe51f37d859f7c68abefb2d7839
toyokokichi <koki0515baske@gmail.com>
2024-02-25 17:43:02 +0900

    商品出品ページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    
+  end
 
   private
 
