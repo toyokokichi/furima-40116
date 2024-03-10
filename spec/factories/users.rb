504487428175f224dd101e3f1f5fadc2b3d84578
toyokokichi <koki0515baske@gmail.com>
2024-03-10 16:20:11 +0900

    formオブジェクトの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { 'test' }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { 'a11111' }
     password_confirmation   { password }
     last_name               { '山田' }
     first_name              { '太郎' }
